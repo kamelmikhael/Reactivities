490047549b2ad0fedb5b9d1e5169440e30b0d35b
Kamel Mikhael Kamel Danial <kamelmikhaelkamel@gmail.com>
2022-01-05 18:36:03 +0200

    End of section 2

---
+++ Persistence/Seed.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public static class Seed
+    {
+        public static async Task SeedActivityData(DataContext context)
+        {
+            if(context.Activities.Any()) return;
+
+            var activities = new List<Activity>()
+            {
+                new Activity
+                {
+                    Title = "Past Activity #1",
+                    Date = DateTime.Now.AddMonths(-2),
+                    Description = "Activity 2 months ago",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Pub"
+                },
+                new Activity
+                {
+                    Title = "Past Activity #2",
+                    Date = DateTime.Now.AddMonths(-1),
+                    Description = "Activity 1 month ago",
+                    Category = "culture",
+                    City = "Paris",
+                    Venue = "Louvre"
+                },
+                new Activity
+                {
+                    Title = "Future Activity #1",
+                    Date = DateTime.Now.AddMonths(1),
+                    Description = "Activity 1 month in the future",
+                    Category = "film",
+                    City = "London",
+                    Venue = "Cinema"
+                },
+            };
+
+            await context.Activities.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
