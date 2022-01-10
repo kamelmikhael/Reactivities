f3d4dc9e925fd94bcae2251582aa347f614e9edd
Kamel Mikhael Kamel Danial <kamelmikhaelkamel@gmail.com>
2022-01-10 17:22:37 +0200

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import Spinner from "../../../app/layout/Spinner";
 import { useStore } from "../../../app/stores/store";
+import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 function ActivityDashboard () {
@@ -23,7 +24,7 @@ function ActivityDashboard () {
             </Grid.Column>
             
             <Grid.Column width={6}>
-                <h2>Activity Filters</h2>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     );
